9c373eb4440f0326476cb0840ec8d94e02071601
Manuel R. Fungate Jr <manuelfungatejr@gmail.com>
2024-04-21 08:06:23 +0200

    check for htop and run or install if not available 2

--- htop_example.sh
+++ htop_example.sh
@@ -5,8 +5,8 @@ cmd=/usr/bin/htop
 if [ -f $cmd ]
 then
         echo "$cmd is available, lets run it..."
-        htop
 else
         echo "$cmd is NOT available"
         sudo apt update && sudo apt install -y htop
 fi
+$cmd
