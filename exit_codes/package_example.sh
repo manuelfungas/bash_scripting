0a8ff60b5532d02d2ee9589b61803513fd14561f
Manuel R. Fungate Jr <manuelfungatejr@gmail.com>
2024-04-21 09:00:13 +0200

    exit codes

--- exit_codes/package_example.sh
+++ exit_codes/package_example.sh
@@ -2,12 +2,12 @@
 
 package=htop
 
-sudo apt install -y $package
+sudo apt install -y $package >> install_results.log
 if [ $? -eq 0]
 then
         echo "the installation of $package was successful"
         echo "The new command is available here:"
         which $package
 else
-        echo "$package failed to install."
+        echo "$package failed to install." >> install_failure.log
 fi
